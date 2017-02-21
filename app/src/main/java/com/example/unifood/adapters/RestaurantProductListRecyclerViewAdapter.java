604b08e20ad103137b866d0f0ab3d1964e464aba
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-02-21 00:17:34 -0300

    Cria lógica (main: fragment) para lista de produtos de um restaurant que o estudante visite. Cria Fragment para lista de produtos Cria Adapter e ViewHolder para essa lista do fragment Cria layouts para produtos e adapter

---
+++ app/src/main/java/com/example/unifood/adapters/RestaurantProductListRecyclerViewAdapter.java
@@ -0,0 +1,75 @@
+package com.example.unifood.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.unifood.R;
+import com.example.unifood.fragments.RestaurantProductListFragment;
+import com.example.unifood.models.Product;
+import java.util.List;
+
+
+public class RestaurantProductListRecyclerViewAdapter extends RecyclerView.Adapter<RestaurantProductListRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Product> mProducts;
+    private final RestaurantProductListFragment.OnListFragmentInteractionListener mListener;
+
+    public RestaurantProductListRecyclerViewAdapter(List<Product> products, RestaurantProductListFragment.OnListFragmentInteractionListener listener) {
+        mProducts = products;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_restaurant_product, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mProduct = mProducts.get(position);
+        holder.mNameView.setText(mProducts.get(position).getName());
+        holder.mDescriptionView.setText(mProducts.get(position).getDescription());
+        holder.mCostView.setText( Float.toString( mProducts.get(position).getCost() ) );
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mProduct);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProducts.size();
+    }
+
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mNameView;
+        public final TextView mDescriptionView;
+        public final TextView mCostView;
+        public Product mProduct;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            mNameView = (TextView) itemView.findViewById(R.id.rest_product_name);
+            mDescriptionView = (TextView) itemView.findViewById(R.id.rest_product_description);
+            mCostView = (TextView) itemView.findViewById(R.id.rest_product_cost);
+        }
+
+    }
+
+}
