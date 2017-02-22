fdec35fc144111fe25b168f57b179e55c0465c07
gabrielmla <gabmla19@gmail.com>
2017-02-22 10:01:14 -0300

    Small fix

--- app/src/main/java/com/example/unifood/fragments/RestaurantProfileFragment.java
+++ app/src/main/java/com/example/unifood/fragments/RestaurantProfileFragment.java
@@ -1,7 +1,7 @@
 package com.example.unifood.fragments;
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +14,29 @@ public class RestaurantProfileFragment extends Fragment {
     TextView restaurantName;
     TextView restaurantUni;
     TextView restaurantLocal;
+    String[] restInfos;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            restInfos = bundle.getStringArray("info");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_restaurant_profile, container, false);
 
-        /*restaurantName = (TextView) getView().findViewById(R.id.rest_profile_name);
-        restaurantUni = (TextView) getView().findViewById(R.id.rest_profile_uni);
-        restaurantLocal = (TextView) getView().findViewById(R.id.rest_profile_local);*/
+        restaurantName = (TextView) rootView.findViewById(R.id.rest_profile_name);
+        restaurantName.setText(restInfos[0]);
+        restaurantUni = (TextView) rootView.findViewById(R.id.rest_profile_uni);
+        restaurantUni.setText(restInfos[1]);
+        restaurantLocal = (TextView) getView().findViewById(R.id.rest_profile_local);
+        restaurantLocal.setText(restInfos[2]);
 
         return rootView;
     }
