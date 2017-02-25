63ee1b76316775b87b897c8d1aad7ff57a254edd
Gabriela Motta <gabriela.oliveira@ccc.ufcg.edu.br>
2017-02-24 21:57:53 -0300

    Finish unit tests for Product, Restaurant and OwnerInfo

--- app/src/test/java/com/example/unifood/models/OwnerInfoTest.java
+++ app/src/test/java/com/example/unifood/models/OwnerInfoTest.java
@@ -1,15 +1,29 @@
 package com.example.unifood.models;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.*;
 
 public class OwnerInfoTest {
 
+    private final String RESTAURANTID = "30102447-3aab-4b11-b159-151e7723ab27";
+    private final String NEW_RESTAURANTID = "30102447-9-151e7723ab27";
+    private OwnerInfo owner;
+
+    @Before
+    public void setUp(){
+        owner = new OwnerInfo(RESTAURANTID);
+    }
+
     @Test
     public void testConstructor(){
-        String restaurantId = "30102447-3aab-4b11-b159-151e7723ab27";
-        OwnerInfo owner = new OwnerInfo(restaurantId);
-        assertEquals("30102447-3aab-4b11-b159-151e7723ab27", owner.getRestaurantId());
+        assertEquals(RESTAURANTID, owner.getRestaurantId());
+    }
+
+    @Test
+    public void testEditOwnerInfo(){
+        owner.setRestaurantId(NEW_RESTAURANTID);
+        assertEquals(NEW_RESTAURANTID, owner.getRestaurantId());
     }
 }
--- app/src/test/java/com/example/unifood/models/RestaurantTest.java
+++ app/src/test/java/com/example/unifood/models/RestaurantTest.java
@@ -7,29 +7,52 @@ import static junit.framework.Assert.*;
 
 public class RestaurantTest {
 
-    Restaurant restaurant;
+    private final String NAME = "Seu Olavo";
+    private final String NEW_NAME = "O&S Burg";
+    private final String CAMPUSID = "49c74eb3-9e4b-41c1-b928-2df460f860b4";
+    private final String NEW_CAMPUSID = "0f841c1-b928-2df4";
+    private final String LOCALIZATION = "Pracinha de humanas";
+    private final String NEW_LOCALIZATION = "Pracinha perto do bloco BG";
+    private final String DESCRIPTION = "Melhor x egg burg da UFCG";
+    private final String USERID = "b159-151e772";
+    private final String NEW_ID = "fh8r6-hhf9w-377y";
+    private final String REVIEWID = "9e4b-41c1-b928";
+    private Restaurant restaurant;
 
     @Before
     public void setUp(){
-       restaurant = new Restaurant("Seu Olavo", "49c74eb3-9e4b-41c1-b928-2df460f860b4", "Pracinha do bloco BG");
+       restaurant = new Restaurant(NAME, CAMPUSID, LOCALIZATION);
     }
 
     @Test
     public void testConstructor(){
-        assertEquals("Seu Olavo", restaurant.getName());
-        assertEquals("49c74eb3-9e4b-41c1-b928-2df460f860b4", restaurant.getCampusId());
-        assertEquals("Pracinha do bloco BG", restaurant.getLocalization());
-
+        assertEquals(NAME, restaurant.getName());
+        assertEquals(CAMPUSID, restaurant.getCampusId());
+        assertEquals(LOCALIZATION, restaurant.getLocalization());
         assertTrue(restaurant.getShortDescription().isEmpty());
-        restaurant.setShortDescription("Melhor x egg burg da UFCG");
-        assertEquals("Melhor x egg burg da UFCG", restaurant.getShortDescription());
+    }
+
+    @Test
+    public void testEditRestaurant(){
+        restaurant.setName(NEW_NAME);
+        restaurant.setCampusId(NEW_CAMPUSID);
+        restaurant.setLocalization(NEW_LOCALIZATION);
+        restaurant.setId(NEW_ID);
+        restaurant.setShortDescription(DESCRIPTION);
+        restaurant.setUserId(USERID);
+
+        assertEquals(NEW_NAME, restaurant.getName());
+        assertEquals(NEW_CAMPUSID, restaurant.getCampusId());
+        assertEquals(NEW_LOCALIZATION, restaurant.getLocalization());
+        assertEquals(NEW_ID, restaurant.getId());
+        assertEquals(DESCRIPTION, restaurant.getShortDescription());
+        assertEquals(USERID, restaurant.getUserId());
     }
 
     @Test
     public void testAddRate(){
         assertTrue(restaurant.getReviewList().isEmpty());
-        restaurant.addRate("9e4b-41c1-b928");
-        assertFalse(restaurant.getReviewList().isEmpty());
-        assertTrue(restaurant.getReviewList().contains("9e4b-41c1-b928"));
+        restaurant.addRate(REVIEWID);
+        assertTrue(restaurant.getReviewList().contains(REVIEWID));
     }
 }
