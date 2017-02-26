04d0478dc3497dc852ad44132d1826acc8019cd0
laybson <laybson.cunha@ccc.ufcg.edu.br>
2017-02-25 21:11:16 -0300

    Adding StudentInfoTest

---
+++ app/src/test/java/com/example/unifood/models/ReviewTest.java
@@ -0,0 +1,13 @@
+package com.example.unifood.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by laybson on 25/02/17.
+ */
+
+public class ReviewTest {
+}
---
+++ app/src/test/java/com/example/unifood/models/StudentInfoTest.java
@@ -0,0 +1,8 @@
+package com.example.unifood.models;
+
+/**
+ * Created by laybson on 25/02/17.
+ */
+
+public class StudentInfoTest {
+}
