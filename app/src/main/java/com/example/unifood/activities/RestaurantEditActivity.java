47622ee4b21c7597082c2f0ca7b9826c1b1e3de9
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-03-04 01:16:24 -0300

    Deleta firebase user e restaurant

--- app/src/main/java/com/example/unifood/activities/RestaurantEditActivity.java
+++ app/src/main/java/com/example/unifood/activities/RestaurantEditActivity.java
@@ -2,20 +2,20 @@ package com.example.unifood.activities;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.unifood.R;
-import com.example.unifood.models.Restaurant;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,9 +23,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -36,15 +33,20 @@ public class RestaurantEditActivity extends AppCompatActivity {
     private FirebaseUser mFirebaseUser;
     private DatabaseReference mDatabase;
     private DatabaseReference restaurantRef;
+    private DatabaseReference userRef;
     private String campus;
     private String restaurantId;
+    private String userId;
 
     public final String VAZIO = "";
 
     @InjectView(R.id.rest_edit_name)  EditText nameField;
     @InjectView(R.id.rest_edit_local) EditText localField;
     @InjectView(R.id.rest_edit_email) EditText emailField;
+    @InjectView(R.id.confirm_password) EditText passwordField;
+    @InjectView(R.id.confirm_email) EditText emailConfirmField;
     @InjectView(R.id.update_b)   Button updateButton;
+    @InjectView(R.id.delete_button) Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,12 +65,14 @@ public class RestaurantEditActivity extends AppCompatActivity {
                 updateInfo();
             }
         });
+        onClickDeleteButton();
     }
 
     public void setUpFirebase(){
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        userId  =  FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
     public void updateInfo() {
@@ -125,10 +129,79 @@ public class RestaurantEditActivity extends AppCompatActivity {
         bt.setEnabled(true);
     }
 
-    public void startHomeActivity(){
+    private void startHomeActivity(){
         Class homeActivity = RestaurantHomeActivity.class;
         Intent goToHome = new Intent(this, homeActivity);
         startActivity(goToHome);
     }
+
+    private void onClickDeleteButton() {
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleterFirebaseUser();
+                deleteUser();
+            }
+        });
+    }
+
+    private void startLoginActivity() {
+        Class loginActivity = LoginActivity.class;
+        Intent goToLogin = new Intent(this, loginActivity);
+        startActivity(goToLogin);
+    }
+
+    private void deleterFirebaseUser() {
+
+        String email = emailConfirmField.getText().toString();
+        String passowrd = passwordField.getText().toString();
+
+        AuthCredential credential = EmailAuthProvider.getCredential(email, passowrd);
+
+        mFirebaseUser.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        mFirebaseUser.delete()
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                    }
+                                });
+                    }
+                });
+
+    }
+
+    private void deleteRestaurant() {
+        restaurantRef = mDatabase.child("restaurants").child(restaurantId);
+        restaurantRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                deleteRestaurant();
+                dataSnapshot.getRef().removeValue();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void deleteUser() {
+        userRef = mDatabase.child("users").child(userId);
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                dataSnapshot.getRef().removeValue();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
 
 }
